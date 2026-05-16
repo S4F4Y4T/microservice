5192ce75543c4da6986da512fd1da820fbfbbbfd
s4f4y4t <safayat.me@gmail.com>
2026-05-16 10:11:15 +0600

    Fix UserHandler service type to pointer and update NewUserHandler signature

--- internals/handler/user.go
+++ internals/handler/user.go
@@ -3,9 +3,9 @@ package handler
 import "microservice/internals/service"
 
 type UserHandler struct {
-	service service.UserService
+	service *service.UserService
 }
 
-func NewUserHandler(service service.UserService) *UserHandler {
+func NewUserHandler(service *service.UserService) *UserHandler {
 	return &UserHandler{service: service}
 }
